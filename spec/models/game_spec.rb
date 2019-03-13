d2dc7e34ab9dd932efdf71b7d6f88bef44b4b401
Edem <keltikan@gmail.com>
2019-03-13 20:42:22 +0300

    Game#take_money! test added

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -33,5 +33,17 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
     end
+
+    it '#take_money! finishes the game' do
+      q = game_w_questions.current_game_question
+      game_w_questions.answer_current_question!(q.correct_answer_key)
+
+      game_w_questions.take_money!
+
+      expect(game_w_questions.status).to eq :money
+      expect(game_w_questions.finished?).to be true
+      expect(game_w_questions.prize).to eq Game::PRIZES.first
+      expect(user.balance).to eq game_w_questions.prize
+    end
   end
 end
