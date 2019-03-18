99da5dd60109f5d8079bf31202b1e6dbff67acf6
Edem <keltikan@gmail.com>
2019-03-18 21:12:39 +0300

    Integration test for creating game added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,4 +68,7 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
+  config.include Warden::Test::Helpers, type: :feature
 end
+
+Capybara.asset_host = 'http://localhost:3000'
