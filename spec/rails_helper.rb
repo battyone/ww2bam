081f8faf1b1fa84fe211caabdaedff86f824d03a
Edem <keltikan@gmail.com>
2019-03-19 19:42:32 +0300

    Devise test helpers updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,8 +66,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers, type: :feature
 end
 
