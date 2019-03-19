47acac5322f09cf23ddd2df2bed70ff3cfd9afea
Edem <keltikan@gmail.com>
2019-03-19 03:57:25 +0300

    users/show view test fixed

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,22 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  before(:each) do
-    assign(:user, FactoryGirl.build_stubbed(:user, name: 'Вован'))
-    assign(:games, [FactoryGirl.build_stubbed(:game, id: 22, created_at: Time.now, current_level: 11)])
+  context 'somebody watches user page' do
+    before(:each) do
+      assign(:user, FactoryGirl.build_stubbed(:user, name: 'Вован'))
+      assign(:games, [FactoryGirl.build_stubbed(:game, id: 22, created_at: Time.now, current_level: 11)])
 
-    render
+      render
+    end
+
+    it 'renders player name' do
+      expect(rendered).to match 'Вован'
+    end
+
+    it 'does not render link for changing registration data' do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+
+    it 'renders games info' do
+      assert_template partial: 'users/_game'
+    end
   end
 
-  it 'renders player name' do
-    expect(rendered).to match 'Вован'
-  end
+  context 'user watches own page' do
+    before(:each) do
+      user = FactoryGirl.create(:user, name: 'Вован')
+      sign_in user
+      assign(:user, user)
 
-  it 'renders link for changing registration data only for relevant user' do
-    expect(rendered).not_to match 'Сменить имя и пароль'
-  end
+      assign(:games, [FactoryGirl.build_stubbed(:game, id: 22, created_at: Time.now, current_level: 11)])
 
-  it 'renders games info' do
-    assert_template partial: 'users/_game'
+      render
+    end
+
+    it 'renders player name' do
+      expect(rendered).to match 'Вован'
+    end
+
+    it 'renders link for changing registration data only for relevant user' do
+      expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    it 'renders games info' do
+      assert_template partial: 'users/_game'
+    end
   end
 end
